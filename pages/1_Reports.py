3d7fc4ecc62174d089ef39dc6c953c80c4abe6f8
Ralph McCracken <ralph@mccracken.work>
2025-02-05 22:27:18 -0600

    reinstating the phase 1 reports

--- pages/1_Reports.py
+++ pages/1_Reports.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 from pymongo import MongoClient
 
@@ -34,35 +35,86 @@ if st.session_state['valid_session']:
     
     l, m, r = st.columns(3)
 
-    report = l.selectbox(label='Choose your report:', options=['Detail','❗️ Comp Review','🏘️ Comp Summary','🏠 Unit Summary'])
+    report = l.selectbox(label='Choose your report:', options=['Detail','❗️ Comp Review','🏘️ Comp Summary','🏠 Unit Summary', '💲 Comp Booking Summary'])
     start_date = m.date_input('Start')
     end_date   = r.date_input('End', min_value=start_date)
     date_range = pd.date_range(start=start_date, end=end_date)
     
 
-    if report == 'Detail':
-        st.info('This is the raw data that is populated with the comp listings script.')
+    match report:
+        case 'Detail':
+            st.info('This is the raw data that is populated with the comp listings script.')
 
-    if report == '❗️ Comp Review':
-        st.info('This is a list of comps that have returned zeros or undefined on specific comp weeks.')
+        case '❗️ Comp Review':
+            st.info('This is a list of comps that have returned zeros or undefined on specific comp weeks.')
         
-    if report == '🏘️ Comp Summary':
-        st.info('This is the per-comp, aggregate average of each non-zero-or-undefined value.')
+        case '🏘️ Comp Summary':
+            st.info('This is the per-comp, aggregate average of each non-zero-or-undefined value.')
     
-    if report == '🏠 Unit Summary':
-        st.info('This is the per-unit, aggregate average of each non-zero-or-undefined comps.')
+        case '🏠 Unit Summary':
+            st.info('This is the per-unit, aggregate average of each non-zero-or-undefined comps.')
+        
+        case _:
+            st.info('Coming soon!')
 
 
     if st.button('Pull Report', use_container_width=True, type='primary'):
 
-        if report == 'Detail':
-            df         = pd.DataFrame(list(database['test'].find({}, {"_id": 0})))
-            df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
-            df         = df[df.Date.isin(date_range)]
-            df['Date'] = pd.to_datetime(df['Date']).dt.date
-            st.dataframe(data=df, hide_index=True, use_container_width=True)
-        else:
-            '**Coming soon!**'
+        match report:
+            case 'Detail':
+                df         = pd.DataFrame(list(database['test'].find({}, {"_id": 0})))
+                df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
+                df         = df[df.Date.isin(date_range)]
+                df['Date'] = pd.to_datetime(df['Date']).dt.date
+                st.dataframe(data=df, hide_index=True, use_container_width=True)
+
+            case '❗️ Comp Review':
+                df         = pd.DataFrame(list(database['test'].find({}, {"_id": 0})))
+                df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
+                df         = df[df.Date.isin(date_range)]
+                df['Date'] = pd.to_datetime(df['Date']).dt.date
+                df         = df.groupby(['Date','Season','Unit','Comp'])[['Total_Rate','Service_Fee','Cost_to_Guest']].agg(np.average)
+                df         = df[df.Cost_to_Guest == 0].reset_index()
+                df         = df[['Season','Unit','Comp']]
+                df         = df.drop_duplicates()
+                st.dataframe(data=df, hide_index=True, use_container_width=True)
+
+            case '🏘️ Comp Summary':
+                df         = pd.DataFrame(list(database['test'].find({}, {"_id": 0})))
+                df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
+                df         = df[df.Date.isin(date_range)]
+                df['Date'] = pd.to_datetime(df['Date']).dt.date
+                df         = df[df.Cost_to_Guest != 0]
+                df         = df.groupby(['Date','Season','Unit','Comp'])[['Total_Rate','Service_Fee','Cost_to_Guest']].agg(
+                    Total_Rate = ('Total_Rate', np.average),
+                    Service_Fee = ('Service_Fee', np.average),
+                    Cost_to_Guest = ('Cost_to_Guest', np.average),
+                    Count = ('Total_Rate','size')
+                )
+                st.dataframe(data=df, use_container_width=True)
+
+            case '🏠 Unit Summary':
+                df         = pd.DataFrame(list(database['test'].find({}, {"_id": 0})))
+                df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
+                df         = df[df.Date.isin(date_range)]
+                df['Date'] = pd.to_datetime(df['Date']).dt.date
+                df         = df[df.Cost_to_Guest != 0]
+                df         = df.groupby(['Date','Season','Unit','Comp'])[['Total_Rate','Service_Fee','Cost_to_Guest']].agg(
+                    Total_Rate = ('Total_Rate', np.average),
+                    Service_Fee = ('Service_Fee', np.average),
+                    Cost_to_Guest = ('Cost_to_Guest', np.average),
+                    Count = ('Total_Rate','size')
+                )
+                df        = df.groupby(['Date','Season','Unit'])[['Total_Rate','Service_Fee','Cost_to_Guest']].agg(
+                    Total_Rate = ('Total_Rate', np.average),
+                    Service_Fee = ('Service_Fee', np.average),
+                    Cost_to_Guest = ('Cost_to_Guest', np.average),
+                    Count = ('Total_Rate','size')
+                )
+                st.dataframe(data=df, use_container_width=True)
+
+            case _:
+                '**Coming soon!**'
     
 
     st.caption('... more in development and coming soon!')
